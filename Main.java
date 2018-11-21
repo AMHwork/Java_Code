7f5da9cd4b9bac6324d1953ec1f460a03e8caf23
AMHwork <45222891+AMHwork@users.noreply.github.com>
2018-11-21 16:37:06 +1100

    AllFactors
    Code for AllFactors

---
+++ Main.java
@@ -0,0 +1,29 @@
+package com.austinhua;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printFactors(6);
+        printFactors(32);
+        printFactors(10);
+        printFactors(-1);
+
+    }
+
+    public static void printFactors (int number){
+
+        if (number < 1){
+            System.out.println("Invalid Value");
+        }
+
+        int counter = 1;
+
+        while (counter <= number){
+            if (number % counter == 0){
+                System.out.println(counter);
+            }
+            counter++;
+        }
+
+    }
+}
